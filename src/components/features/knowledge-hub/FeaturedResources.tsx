516b626d57d60c4a1a81617d5fbe7ececd198b94
Andika Kurniawan <andikaflying@gmail.com>
2026-08-12 11:36:49 +0700

    feat: change icon

--- src/components/features/knowledge-hub/FeaturedResources.tsx
+++ src/components/features/knowledge-hub/FeaturedResources.tsx
@@ -6,13 +6,10 @@ import { cn } from '@/utils/cn';
 
 function PinIcon() {
   return (
-    <svg aria-hidden="true" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="21" viewBox="0 0 14 21" fill="none">
       <path
-        d="M12 2v6.5M12 8.5 6 12l6 2.5m0-6 6 3.5-6 2.5m0 0V22"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d="M10.6945 11.0227L12.1087 13.4722L11.5911 15.404L6.76146 14.1099L5.20854 19.9055L3.9838 20.6126L3.27669 19.3879L4.82961 13.5923L-2.35438e-05 12.2982L0.517615 10.3664L2.9671 8.95215L4.77884 2.19067L3.81291 1.93185L4.33055 -5.36442e-07L13.9898 2.58819L13.4722 4.52004L12.5062 4.26122L10.6945 11.0227ZM3.2705 11.104L9.35584 12.7346L8.54266 11.3261L10.5744 3.74358L6.71069 2.70831L4.67896 10.2908L3.2705 11.104Z"
+        fill="white"
       />
     </svg>
   );
--- src/components/features/knowledge-hub/KnowledgeHubHero.tsx
+++ src/components/features/knowledge-hub/KnowledgeHubHero.tsx
@@ -2,6 +2,30 @@ import Image from 'next/image';
 import { Container } from '@/components/layout/Container';
 import { Typography } from '@/components/common/design-system/Typography';
 
+function AccentMark() {
+  return (
+    <span
+      aria-hidden="true"
+      className="absolute top-[-1.25rem] left-[0.75rem] -z-10 -ml-6 hidden h-7 w-6 shrink-0 md:block"
+    >
+      <Image
+        src="/icons/accent-square-1.svg"
+        alt=""
+        width={14}
+        height={14}
+        className="absolute top-[51%] right-[90%]"
+      />
+      <Image
+        src="/icons/accent-square-2.svg"
+        alt=""
+        width={15}
+        height={14}
+        className="absolute top-0 left-[0%]"
+      />
+    </span>
+  );
+}
+
 export function KnowledgeHubHero() {
   return (
     <div className="bg-dark relative isolate -z-10 h-[26rem] overflow-hidden sm:h-[32rem] lg:h-[688px]">
@@ -22,21 +46,22 @@ export function KnowledgeHubHero() {
         className="pointer-events-none absolute top-0 right-0 hidden select-none lg:block"
       />
       <Container className="relative flex min-h-[16rem] flex-col justify-center gap-3 py-10 sm:min-h-[20rem] sm:gap-4 sm:py-12 lg:min-h-[28.5rem] lg:py-16">
-        <Image
+        {/* <Image
           src="/icons/accent-1.svg"
           alt=""
           width={24}
           height={28}
           aria-hidden="true"
           className="hidden lg:block"
-        />
+        /> */}
         <Typography
           as="h1"
           weight="bold"
           color="inherit"
           size="6xl"
-          className="text-3xl tracking-[-0.05rem] text-white uppercase lg:text-6xl lg:tracking-[-0.1875rem]"
+          className="relative text-3xl tracking-[-0.05rem] text-white uppercase lg:text-6xl lg:tracking-[-0.1875rem]"
         >
+          <AccentMark />
           Knowledge Hub
         </Typography>
         <Typography
